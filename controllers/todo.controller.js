b584888390c723a5ecf9580f5d1bb0a7fa2b809d
Martin Kregar <martinkregar@gmail.com>
2020-01-03 17:15:24 +0100

    added more tests

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,5 +1,6 @@
 const TodoModel = require('../model/todo.model');
 
-exports.createTodo = () => {
-    TodoModel.create();
+exports.createTodo = (req, res, next) => {
+    const createdTodo = TodoModel.create(req.body);
+    res.status(201).json(createdTodo);
 };
